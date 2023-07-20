bb46c1ab3430902c130179140c1f1876759ffb60
SahilNimse <sahilnimse088@gmail.com>
2023-07-20 14:07:13 +0530

    first commit

---
+++ dayone/src/org/tnsif/ComilerInput.java
@@ -0,0 +1,13 @@
+//program to demonstrate on compile-time-input
+package org.tnsif;
+
+public class ComilerInput {
+
+	public static void main(String[] args) {
+		//variable initialization
+		short num=34;
+		System.out.println("The num is: "+num);
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/CompilerTwo.java
@@ -0,0 +1,15 @@
+package org.tnsif;
+import java.util.Scanner;
+public class CompilerTwo {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter a string: ");
+	String strl=s.nextLine();
+	
+	System.out.println("String is: "+strl);
+	s.close();
+	
+	}
+
+}
